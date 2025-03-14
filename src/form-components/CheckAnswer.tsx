f38762f3d72ec0ac80e11eae99fe8497a3c64a2c
ThomasLavallee <lavallee@udel.edu>
2025-03-14 19:33:36 -0400

    Finished + tested CheckAnswer Form

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    // State of the user's answer in the textbox
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group id="formCheckAnswer">
+                <Form.Label>Check Answer:</Form.Label>
+                <Form.Control
+                    value={userAnswer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setUserAnswer(event.target.value);
+                    }}
+                ></Form.Control>
+            </Form.Group>
+
+            {userAnswer === expectedAnswer ?
+                <span>✔️</span>
+            :   <span>❌</span>}
         </div>
     );
 }
