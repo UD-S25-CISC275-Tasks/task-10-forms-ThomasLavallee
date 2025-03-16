fc3ba03a369d375a318db9966b28f15ec209fa79
ThomasLavallee <lavallee@udel.edu>
2025-03-15 20:30:51 -0400

    Finished + tested ChangeColor Form Component

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 import { GiveAttempts } from "./form-components/GiveAttempts";
-//import { EditMode } from "./form-components/EditMode";
+import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-//import { ChangeColor } from "./form-components/ChangeColor";
+import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): React.JSX.Element {
     return (
@@ -27,9 +27,9 @@ function App(): React.JSX.Element {
             <hr></hr>
             <GiveAttempts></GiveAttempts>
             <hr></hr>
-            {/*<EditMode></EditMode>*/}
+            <EditMode></EditMode>
             <hr></hr>
-            {/*<ChangeColor></ChangeColor>*/}
+            <ChangeColor></ChangeColor>
             <hr></hr>
             <MultipleChoiceQuestion
                 options={["a", "b", "c"]}
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,72 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    // Current mode state
+    const [isEditMode, setMode] = useState<boolean>(false);
+
+    // User's name state
+    const [userName, setUserName] = useState<string>("Your Name");
+
+    // User's student status state
+    const [isStudent, setStudent] = useState<string>("student");
+
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudent(event.target.value);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Group id="editStudentForm">
+                <Form.Switch
+                    type="switch"
+                    label="Toggle Edit Mode"
+                    checked={isEditMode}
+                    onChange={() => {
+                        setMode(!isEditMode);
+                    }}
+                ></Form.Switch>
+            </Form.Group>
+
+            {isEditMode ?
+                <Form.Group>
+                    <Form.Label>Edit Student Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={userName}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>,
+                        ) => {
+                            setUserName(event.target.value);
+                        }}
+                    ></Form.Control>
+
+                    <Form.Label>Student Status</Form.Label>
+                    <Form.Check
+                        inline
+                        type="checkbox"
+                        name="student"
+                        label="Student"
+                        value="student"
+                        onChange={updateStudent}
+                        checked={isStudent === "student"}
+                    ></Form.Check>
+                    <Form.Check
+                        inline
+                        type="checkbox"
+                        name="student"
+                        label="Not a Student"
+                        value="not a student"
+                        onChange={updateStudent}
+                        checked={isStudent !== "student"}
+                    ></Form.Check>
+                </Form.Group>
+            :   <span>
+                    {userName} is {isStudent === "student" ? "" : "not"} a
+                    student
+                </span>
+            }
         </div>
     );
 }
